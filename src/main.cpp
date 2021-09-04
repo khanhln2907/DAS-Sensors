54a0d978183bf762f30bb5821c7113167f20d2df
KhanhLn <khanhln2907@gmail.com>
2021-09-04 22:05:58 +0200

    setup Teensy 36 working environment

---
+++ src/main.cpp
@@ -0,0 +1,14 @@
+#include <Arduino.h>
+#include <BasicType.h>
+
+void setup() {
+  // put your setup code here, to run once:
+  Attitude_t rate;
+  rate.phi = 0;
+  rate.theta = 0;
+  rate.psi = 0;
+}
+
+void loop() {
+  // put your main code here, to run repeatedly:
+}
